41eab3ebd37edf9bfed68176314a226a745532d1
claudiobosetti <claudiobosetti1@gmail.com>
2020-12-01 13:56:52 +0100

    favicon fixed, scrollbar added

---
+++ js/scrollbar.js
@@ -0,0 +1,7 @@
+let progressBar = document.querySelector("#progressBar");
+let totalPageHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = () => {
+    let newProgressHeight = (window.pageYOffset / totalPageHeight) * 100;
+    progressBar.style.height = `${newProgressHeight}%`;
+    progressBar.style.opacity = `${newProgressHeight}%`;
+}
